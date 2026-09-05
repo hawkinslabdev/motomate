fb2686f8c010eb53e9658e2c7b63ea87fed3c90a
hawkinslabdev <59891413+hawkinslabdev@users.noreply.github.com>
2026-09-05 12:13:39 +0200

    chore: run lint

--- motomate/src/routes/(auth)/login/+page.svelte
+++ motomate/src/routes/(auth)/login/+page.svelte
@@ -155,9 +155,7 @@
 								stroke-linejoin="round"
 								aria-hidden="true"
 							>
-								<path
-									d="M2 12s3.5-7 10-7 10 7 10 7-3.5 7-10 7-10-7-10-7Z"
-								/>
+								<path d="M2 12s3.5-7 10-7 10 7 10 7-3.5 7-10 7-10-7-10-7Z" />
 								<circle cx="12" cy="12" r="3" />
 							</svg>
 						{:else}
